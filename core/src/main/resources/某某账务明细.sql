13a75feebacf876326eb9544c996ca97dc07e140
bbyshp@126.com <hanlin123>
2017-04-20 15:33:45 +0800

    test

---
+++ core/src/main/resources/某某账务明细.sql
@@ -0,0 +1,2 @@
+select t.xxx 合同号, t.ggg , t.www 合同时间, t.vvv 还款金额
+  from t_eesdsfsf t where  rownum<6100
--- core/src/main/resources/还款记录.sql
+++
@@ -1,2 +0,0 @@
-select t.serialno 合同号, t.objectno , t.paydate 合同时间, t.payprincipalamt 还款金额
-  from ACCT_PAYMENT_SCHEDULE t where  rownum<6100
