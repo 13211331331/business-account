f445f0681b28a3b573836eb63a7966d07c73db17
bbyshp@126.com <hanlin123>
2017-05-24 10:41:48 +0800

    test

---
+++ osjp-spring-batch-example/src/main/java/com/geekcap/javaworld/springbatchexample/simple/task/Task02.java
@@ -0,0 +1,17 @@
+package com.geekcap.javaworld.springbatchexample.simple.task;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+/**
+ * Created by hanlin.huang on 2017/5/19.
+ */
+public class Task02 implements Tasklet {
+    @Override
+    public RepeatStatus execute(StepContribution stepcontribution, ChunkContext chunkcontext) throws Exception {
+        System.out.println("doing Task02......................");
+        return RepeatStatus.FINISHED;
+    }
+}
